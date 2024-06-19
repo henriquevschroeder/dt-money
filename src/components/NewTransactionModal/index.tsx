4ad5e3860c2c6ed0f41be1c6d0af55334016a826
henriquevschroeder <henriquevschdev@gmail.com>
2024-06-18 23:40:57 -0300

    feat: Add NewTransactionModal component with @radix-ui for accessibility

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,29 @@
+import * as Dialog from '@radix-ui/react-dialog';
+import { CloseButton, Content, Overlay } from './styles';
+import { X } from 'phosphor-react';
+
+export function NewTransactionModal() {
+  return (
+    <Dialog.Portal>
+      <Overlay />
+
+      <Content>
+        <Dialog.Title>New transaction</Dialog.Title>
+
+        <CloseButton>
+          <X />
+        </CloseButton>
+
+        <form action="">
+          <input type="text" placeholder="Description" required />
+          <input type="number" placeholder="Price" required />
+          <input type="text" placeholder="Category" required />
+
+          <button type="submit">
+            Add
+          </button>
+        </form>
+      </Content>
+    </Dialog.Portal>
+  )
+}
